45605229328eb5d7d3e02e9e50ffd4489d191b28
Sonia Chevli <soniachevli@gmail.com>
2018-08-20 22:28:35 +0000

    added endpoint stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,22 @@
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status', strict_slashes=False)
 def get_status():
     "returns status ok, 200"
     return jsonify({'status':'ok'}), 200
+
+
+@app_views.route('/stats', strict_slashes=False)
+def get_stats():
+    '''add count method from storage '''
+    new_dict = {}
+    new_dict["amenities"] = storage.count("Amenity")
+    new_dict["cities"] = storage.count("City")
+    new_dict["places"] = storage.count("Place")
+    new_dict["reviews"] = storage.count("Review")
+    new_dict["states"] = storage.count("State")
+    new_dict["users"] = storage.count("User")
+
+    return jsonify(new_dict), 200
