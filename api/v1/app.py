ca8df83afcf1a29aa09fb2867037e6d28dceac0e
IOLevi <337@holbertonschool.com>
2018-08-20 21:28:52 +0000

    working through problem 4

---
+++ api/v1/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
+@app.teardown_appcontext
+def tear_down():
+    "tears down"
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host=os.getenv("HBNB_API_HOST") if os.getenv("HBNB_API_HOST") else "0.0.0.0", port=os.getenv("HBNB_API_PORT") if os.getenv("HBNB_API_PORT") else 5000, threaded=True)
