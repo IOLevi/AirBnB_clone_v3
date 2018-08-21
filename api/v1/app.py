867cdb93d715bfd4a9124002f4abe9599d52b65d
Sonia Chevli <soniachevli@gmail.com>
2018-08-21 01:28:59 +0000

    Add 404 error message

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -10,6 +10,10 @@ app.register_blueprint(app_views, url_prefix="/api/v1")
 def tear_down(self):
     "tears down"
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     app.run(host=os.getenv("HBNB_API_HOST") if os.getenv("HBNB_API_HOST") else "0.0.0.0", port=os.getenv("HBNB_API_PORT") if os.getenv("HBNB_API_PORT") else 5000, threaded=True)
