989d7ced9f3d0aa2bf31a680d28f2163d22977aa
Sonia Chevli <soniachevli@gmail.com>
2018-08-23 16:03:38 +0000

    make json pretty

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ import os
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views, url_prefix="/api/v1")
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
